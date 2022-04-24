3628ea165265efed5b08f67523d2d4e7c7a4b9c7
Abdallah Mahmoud <abdullahmahmoud4550@gmail.com>
2022-04-24 04:38:00 +0200

    Update user home page

--- Courses/Controllers/HomeController.cs
+++ Courses/Controllers/HomeController.cs
@@ -28,6 +28,10 @@ namespace Courses.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
